7acb61b237431f3cc02e0056bc69096662692eba
Nasir Gaayte <slowboyz12@gmail.com>
2025-10-14 16:13:23 +0300

    add HomeView

---
+++ PleaseDo/PleaseDo/Views/HomeView.swift
@@ -0,0 +1,56 @@
+//
+//  HomeView.swift
+//  PleaseDo
+//
+//  Created by nasir on 14/10/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    
+    @State private var path = NavigationPath()
+    
+    
+    var body: some View {
+        NavigationStack {
+            TabView {
+                Text("page1")
+                Text("page2")
+                Text("page3")
+            }
+            .tabViewStyle(.page)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading
+                ){
+                    Button {
+                        print("logout")
+                    } label: {
+                        Image(systemName: "person.crop.circle")
+                        
+                    }
+                }
+                ToolbarItem(placement: .topBarLeading
+                ){
+                    Button {
+                        print("Navigate to new Item")
+                    } label: {
+                        Image(systemName: "plus")
+                        
+                    }
+                }
+            }
+            .navigationDestination(for: String.self
+            ) { _ in
+                Text("New Item")
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    HomeView()
+}
