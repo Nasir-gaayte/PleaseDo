327be66a8360830b81f6062f1a5ea5a97a4b144c
Nasir Gaayte <slowboyz12@gmail.com>
2025-10-14 08:19:17 +0300

    add project

---
+++ PleaseDo/PleaseDo/PleaseDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoApp.swift
+//  PleaseDo
+//
+//  Created by nasir on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
